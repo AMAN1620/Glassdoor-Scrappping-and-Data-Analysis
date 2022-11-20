d2b34b8733fc9d8ca2c025c5b349f89b82c96bc3
Aman Kumar <aman.asingh307@gmail.com>
2022-11-20 12:21:41 +0530

    Worked on Scrapper

---
+++ glassdoor_analysis.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 13 12:05:47 2022
+
+@author: amana
+"""
+
+import pandas as pd
+
+import glassdoor_scrapper as gs
+import glassdoor_scrapper_final as gsf
+path = 'C:/Users/amana/OneDrive/Desktop/glassdoor_salary_analysis/chromedriver'
+
+df = gs.get_jobs("Data Science",5,False,20)
+
+print(df)
